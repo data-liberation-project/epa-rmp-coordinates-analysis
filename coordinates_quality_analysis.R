e9eb09ed21d712a6e8a77a3bd38a64fb5e7d4c5f
Adrienne Damicis <adamicis@phi.org>
2023-04-28 10:30:31 -0400

    updated facilities spreadsheet and analysis

--- coordinates_quality_analysis.R
+++ coordinates_quality_analysis.R
@@ -3,6 +3,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 options(scipen=999)
+options(tigris_use_cache = TRUE)
 
 library(dplyr)
 library(tidyverse)
@@ -10,6 +11,22 @@ library(sf)
 library(tigris)
 library(tidygeocoder)
 library(tmap)
+library(cdlTools)
+library(stringr)
+library(kableExtra)
+
+### read in data files
+#facilities <- read.csv("https://raw.githubusercontent.com/data-liberation-project/epa-rmp-spreadsheets/main/data/output/facilities.csv")
+#submissions <- read.csv("https://raw.githubusercontent.com/data-liberation-project/epa-rmp-spreadsheets/main/data/output/submissions.csv")
+
+#write.csv(facilities, file = "data/files/facilities.csv", row.names=F)
+#write.csv(submissions, file = "data/files/submissions.csv", row.names=F)
+
+#facilities_dictionary <- read.csv("https://raw.githubusercontent.com/data-liberation-project/epa-rmp-spreadsheets/main/data/dictionaries/facilities.csv")
+#submissions_dictionary <- read.csv("https://raw.githubusercontent.com/data-liberation-project/epa-rmp-spreadsheets/main/data/dictionaries/submissions.csv")
+
+#write.csv(facilities_dictionary, file = "data/dictionaries/facilities.csv", row.names=F)
+#write.csv(submissions_dictionary, file = "data/dictionaries/submissions.csv", row.names=F)
 
 ### read in data files
 facilities <- read.csv("data/files/facilities.csv")
@@ -23,10 +40,94 @@ submissions <- read.csv("data/files/submissions.csv")
 facilities_missing <- facilities %>%
   filter(is.na(Lat)==T | is.na(Lng)==T)
 
-### quantify missingness in facility spreadsheet
 length(unique(facilities_missing$EPAFacilityID))
-length(unique(facilities_missing$EPAFacilityID)) / length(unique(facilities$EPAFacilityID)) * 100
-#5092 facilities missing coordinates (24.2% of all facilities in database)
+# 0 facilities missing lat/long data
+
+rm(facilities_missing)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# identify data quality concerns in Lat/Lng for facilities spreadsheet
+# (i.e. facilities with Lat/Lng outside of the US, reported state, county or zip)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+###convert facilities to sf dataframe
+facilities <- st_as_sf(facilities, coords = c("Lng", "Lat"), crs=4269)
+
+### load shapefiles for US county boundaries
+counties <- counties(cb=TRUE)
+
+### join facilities with counties
+fac <- st_join(facilities, counties, join=st_within)
+
+### recode state initials as FIPS codes, add leading 0 to CountyFIPS, and drop unnecessary fields
+fac <- fac %>%
+  mutate(State_FIPS = str_pad(fips(State, to="FIPS"), width=2, side="left", pad="0"),
+         CountyFIPS = str_pad(CountyFIPS, width=5, side="left", pad="0")) %>%
+  select(c(EPAFacilityID,
+           City,
+           State,
+           ZipCode,
+           CountyFIPS,
+           LatestReceiptDate,
+           STATEFP,
+           COUNTYFP,
+           GEOID,
+           State_FIPS))
+
+### create new field with type of disagreement (1 = not in US, 2 = not in state, 3 = not in county)
+fac <- fac %>%
+  mutate(disagree = as.factor(case_when(is.na(STATEFP)==T ~ 1,
+                                        as.character(State_FIPS) != STATEFP ~ 2,
+                                        as.character(CountyFIPS) != GEOID ~ 3,
+                                        .default=NA)))
+
+### interactive map of submission coordinates
+us <- nation(resolution = "5m")
+
+tmap_mode("view")
+m1 <- tm_basemap("CartoDB.PositronNoLabels") + 
+  tm_scale_bar() + 
+  tm_shape(us, unit="imperial") +
+  tm_borders(col="black") +
+  tm_shape(fac) +
+  tm_dots(col="disagree",
+          size = 0.02,
+          alpha = 0.8,
+          title="Facility Coordinates: Disagreement with Reported Address",
+          labels=c("Out of US", "Out of State", "Out of County"),
+          textNA="Agrees") +
+  tm_tiles("CartoDB.PositronOnlyLabels") 
+m1
+
+### output table of results
+fac_print <- fac %>%
+  filter(is.na(disagree)==F) %>%
+  arrange(disagree, desc(LatestReceiptDate))
+t1 <- kable(fac_print, format="html", booktabs = TRUE) %>%
+  kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))
+
+
+fac_sum <- fac_print %>%
+  st_drop_geometry() %>%
+  group_by(disagree) %>%
+  summarize(n = n(),
+            perc = round(n/length(facilities$EPAFacilityID)*100, 2))
+t2 <- kable(fac_sum, format="html", booktabs = TRUE) %>%
+  kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))
+
+
+### try reverse geocoding coordinates to get city and zipcode
+facilities_test <- facilities %>%
+  filter(str_detect(LatestReceiptDate, "^2022")) 
+
+facilities_test[,c("Lng", "Lat")] <- st_coordinates(facilities_test)
+
+facilities_test <- facilities_test %>%
+  reverse_geocode(lat=Lat, long=Lng, method="arcgis", full_results=TRUE)
+
+
+
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # investigate submissions spreadsheet
